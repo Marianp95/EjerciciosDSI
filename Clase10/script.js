92f10472c494e369e338fe603c01e5b8b08442ca
MariaNietoPetinal <100316475@alumnos.uc3m.es>
2018-03-14 12:17:19 +0100

    Javascript ejercicio clase

--- Clase10/script.js
+++ Clase10/script.js
@@ -1,4 +1,13 @@
-var app = angular.module('basicApp', []);
-app.controller('homeController', function($scope){
-    $scope.uname="Hola Mundo";
-})
+var module = angular.module('listApp', []);
+var controller = module.controller('listController', initController);
+
+function initController ($scope) {
+    $scope.items=['first Item'];
+    $scope.addItem = function(){
+        if($scope.newItem){
+            $scope.items.push($scope.newItem);
+            $scope.newItem= undefined;
+        }
+
+    }
+}
