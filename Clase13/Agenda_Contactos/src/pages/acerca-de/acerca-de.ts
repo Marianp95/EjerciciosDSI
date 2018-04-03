5506655ed23256e7e86b32730e22c802c265bf4c
MariaNietoPetinal <100316475@alumnos.uc3m.es>
2018-04-04 00:41:13 +0200

    Navegación incluido

--- Clase13/Agenda_Contactos/src/pages/acerca-de/acerca-de.ts
+++ Clase13/Agenda_Contactos/src/pages/acerca-de/acerca-de.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import {ContactosPage} from "../contactos/contactos";
-import { ContactosPage } from "../contactos/contactos";
 
 
 /**
